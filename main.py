aee68587723ef3a79b6c0fce1241c9f8b25c8236
Martin Sørensen <maros105@gmail.com>
2021-04-11 22:00:33 +0200

    Demo release

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 # Kivy
 from kivy.app import App
 from kivy.lang import Builder
-# Kivy MapView stuff
+# Kivy MapView
 from kivy_garden.mapview import MapView, MapMarkerPopup
 # Kivy properties
-from kivy.properties import StringProperty
-
-class PointOfInterest():
-    def __init__(self, latitude, longitude, title='POI', description=''):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.title = title
-        self.description = description
+from kivy.properties import NumericProperty, StringProperty
 
 class Content(MapView):
     def build(self):
         return self
 
-class SchoolMarkerPopup(MapMarkerPopup):
-    marker = 'schoolmarker.png'
+class POIMarkerPopup(MapMarkerPopup):
+    latitude = NumericProperty()
+    longitude = NumericProperty()
+    title = StringProperty()
+    description = StringProperty()
+    imageUrl = StringProperty()
 
 class MapApp(App):
      def build(self):
-         root_widget = Builder.load_file('mapViewGUI.kv')
+         with open('mapViewGUI.kv', encoding='utf8') as f:
+             root_widget = Builder.load_string(f.read())
          root_widget.build()
-         mymarker = SchoolMarkerPopup(lat=55.015133314076266, lon=11.916701493737868)
-         root_widget.add_marker(mymarker)
          return root_widget
 
 if __name__ in ('__main__', '__android__'):
